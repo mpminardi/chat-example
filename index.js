0e710b70207f541754a5e02681a9f6e82cc62772
Mario Minardi <mminardi@shaw.ca>
2017-03-10 21:15:49 -0700

    Work for SENG513 assignment 3

--- index.js
+++ index.js
@@ -1,18 +1,115 @@
 var express = require('express');
+var cookieParser = require('cookie-parser');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var CircularBuffer = require("circular-buffer");
+var dateFormat = require('dateformat');
+
+// Buffer for the messages
+var messageBuffer = new CircularBuffer(250);
+
+var uniqueUsers = [];
+var onlineUsers = [];
 
 http.listen( port, function () {
     console.log('listening on port', port);
 });
 
+app.use(cookieParser());
+
+// Generate username / color on connection if needed
+app.get('/', function(req, res, next) {
+    if (req.cookies.user === '' || req.cookies.user === undefined) {
+        res.cookie('user', 'User' + uniqueUsers.length);
+    }
+
+    if (req.cookies.color == '' || req.cookies.color === undefined) {
+        res.cookie('color', '#ffffff');
+    }
+
+    next();
+});
+
 app.use(express.static(__dirname + '/public'));
 
 // listen to 'chat' messages
-io.on('connection', function(socket){
-    socket.on('chat', function(msg){
-	io.emit('chat', msg);
+io.on('connection', function(socket) {
+    // Emit the chatlog when a user first connects
+    socket.emit('chatlog', messageBuffer.toarray());
+
+    socket.on('chat', function(msg, name, color){
+        let changeColorCommand = "/nickcolor";
+        let changeNameCommand = "/nick";
+        let splitMsg = msg.split(" ");
+
+        if (changeColorCommand === splitMsg[0]) {
+            let newColor = splitMsg[1];
+
+            //If this is a valid hex color
+            if (/^[0-9A-F]{6}$/i.test(newColor)) {
+                socket.emit('recolor', '#'+ newColor);
+            } else {
+                socket.emit('errorMsg', newColor + ' is not a valid color! Please enter a color of the form RRGGBB');
+            }
+
+        } else if (changeNameCommand === splitMsg[0]) {
+            let newName = splitMsg[1];
+            let newNamePos = uniqueUsers.indexOf(newName);
+
+            // Update lists and rename the user if this is an unused name.
+            if (-1 === newNamePos) {
+                uniqueUsers.splice(uniqueUsers.indexOf(socket.user), 1);
+                uniqueUsers.push(newName);
+                onlineUsers.splice(onlineUsers.indexOf(socket.user), 1);
+                onlineUsers.push(newName);
+
+                socket.emit('rename', newName);
+                io.emit('userlistChange', onlineUsers);
+
+                socket.user = newName;
+            // Otherwise alert the user that this name is taken.
+            } else {
+                socket.emit('errorMsg', 'The nickname ' + newName + ' is already taken!');
+            }
+        } else {
+            let msgData = { time: getTimeString(), color: color, user: socket.user, msg: msg };
+
+            messageBuffer.push(msgData);
+            io.emit('chat', msgData);
+        }
+    });
+
+    // Set the username of this socket.
+    socket.on('username', function(name) {
+        socket.user = name;
+
+        socket.emit('rename', name);
+        let namePos = uniqueUsers.indexOf(name);
+
+        if (-1 === namePos) {
+            uniqueUsers.push(name);
+        }
+
+        onlineUsers.push(name);
+        io.emit('userlistChange', onlineUsers);
+    });
+
+    // Remove user from online user list and update clients.
+    socket.on('disconnect', function() {
+        let userIdx = onlineUsers.indexOf(socket.user);
+
+        if (-1 != userIdx) {
+            onlineUsers.splice(userIdx, 1);
+        }
+
+        io.emit('userlistChange', onlineUsers);
     });
 });
+
+// Returns the current time
+function getTimeString() {
+    let now = new Date();
+    return dateFormat(now, 'h:MM TT');
+}
--- public/client.js
+++ public/client.js
@@ -1,12 +1,77 @@
 // shorthand for $(document).ready(...)
 $(function() {
     var socket = io();
+
+    // Set socket username for server
+    socket.emit('username', getCookie('user'));
+
     $('form').submit(function(){
-	socket.emit('chat', $('#m').val());
+	socket.emit('chat', $('#m').val(), getCookie('user'), getCookie('color'));
 	$('#m').val('');
 	return false;
     });
-    socket.on('chat', function(msg){
-	$('#messages').append($('<li>').text(msg));
+
+    // Output chat history
+    socket.on('chatlog', function(msgArray) {
+        for (idx in msgArray) {
+            $('#messages').append($('<li>').append(formatMsg(msgArray[idx])));
+        }
+    });
+
+    // Update chat window
+    socket.on('chat', function(msg) {
+        $('#messages').append($('<li>').append(formatMsg(msg)));
+    });
+
+    // Update user cookie
+    socket.on('rename', function(name) {
+	document.cookie = 'user=' + name;
+        $('#username').text('You are ' + name);
+    });
+
+    // Update color cookie
+    socket.on('recolor', function(color) {
+        let msg = '<span style="color:' + color + '"> This is your new nickname color! </span>';
+        document.cookie = 'color=' + color;
+        $('#messages').append($('<li>').append(msg));
+    });
+
+    // Update user list
+    socket.on('userlistChange', function(userList) {
+        $('#users').empty();
+        for (idx in userList) {
+            $('#users').append($('<li>').text(userList[idx]));
+        }
+    });
+
+    // Output error message
+    socket.on('errorMsg', function(msg) {
+        $('#messages').append($('<li>').append(msg));
     });
 });
+
+// Parses out the given cookie name
+function getCookie(name) {
+    let val = "";
+    let splitCookie = ('; ' + document.cookie + ';').split('; ' + name + '=');
+
+    if (splitCookie.length === 2) {
+        val = splitCookie[1].substring(0, splitCookie[1].indexOf(";"));
+    }
+
+    return val;
+}
+
+// Formats a msg for displaying in the chatlog
+function formatMsg(data) {
+    let msgStyleStart = '<i>';
+    let msgStyleEnd = '</i>';
+
+    if (data.user === getCookie('user')) {
+        msgStyleStart = '<b>';
+        msgStyleEnd = '</b>';
+    }
+
+    let msg = data.time + '<span style="color:' + data.color + '"> ' + data.user + ' : </span>' + data.msg;
+    return msgStyleStart + msg + msgStyleEnd;
+}
